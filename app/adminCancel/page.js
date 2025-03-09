0809ff16fa9d25153ce23eed72f94c5329c37915
joySutradhor <joysutradhorcmt@gmail.com>
2025-03-09 10:07:40 +0600

    order cancel route

--- app/adminCancel/page.js
+++ app/adminCancel/page.js
@@ -5,14 +5,15 @@ import { FaTrash } from 'react-icons/fa'
 import { LuAlarmClock } from 'react-icons/lu'
 import { SlCalender } from 'react-icons/sl'
 import Swal from 'sweetalert2'
-
+import { GrUserAdmin } from 'react-icons/gr'
 import Link from 'next/link'
-import { FaInfoCircle } from 'react-icons/fa'
+import { IoCheckmarkDoneCircleSharp } from 'react-icons/io5'
 import { VscCheckAll } from 'react-icons/vsc'
-import { ImRadioChecked } from 'react-icons/im'
+import { PiChecksBold } from 'react-icons/pi'
 import { useRouter } from 'next/navigation'
+import { FaInfoCircle } from 'react-icons/fa'
 
-function AdminDashboard () {
+function AdminCancel () {
   const [bookings, setBookings] = useState([])
   const [loading, setLoading] = useState(false)
 
@@ -21,20 +22,21 @@ function AdminDashboard () {
   useEffect(() => {
     const isAdmin = localStorage.getItem('isAdmin')
     if (!isAdmin) {
-      router.push('/adminLogin') // Redirect to login if not authenticated
+      router.push('/adminLogin')
     }
   }, [])
 
   useEffect(() => {
     setLoading(true)
     axios
-      .get(`https://esthetic-serverside-teal.vercel.app/api/orders`)
+      .get(`https://esthetic-serverside-teal.vercel.app/api/orders/cancel`)
       .then(res => {
         setBookings(res.data)
         setLoading(false)
       })
       .catch(err => {
-        setLoading(false)
+        console.error('Error fetching data:', err)
+        setLoading(false) // Ensure loading is turned off even if there's an error
       })
   }, [])
 
@@ -75,92 +77,34 @@ function AdminDashboard () {
   }
 
   //   status update here
-  const updateStatus = orderId => {
-    Swal.fire({
-      title: 'Are you want to Accept?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, Accept it!'
-    }).then(result => {
-      if (result.isConfirmed) {
-        // Update the bookings state locally before making the API call
-        const updatedBookings = bookings.filter(order => order._id !== orderId)
-        setBookings(updatedBookings)
+  // const updateStatus = orderId => {
+  //   Swal.fire({
+  //     title: 'Are you sure you want to Accept?',
+  //     text: "You won't be able to revert this!",
+  //     icon: 'warning',
+  //     showCancelButton: true,
+  //     confirmButtonColor: '#3085d6',
+  //     cancelButtonColor: '#d33',
+  //     confirmButtonText: 'Yes, Accept it!'
+  //   }).then(result => {
+  //     if (result.isConfirmed) {
+  //       axios
+  //         .patch(`https://esthetic-serverside-teal.vercel.app/api/update/${orderId}`)
+  //         .then(res => {})
+  //         .catch(err => {
+  //           console.error('Error aceepting order:', err)
+  //           // If error occurs, revert the local state update
+  //           setBookings(bookings)
+  //         })
 
-        axios
-          .patch(
-            `https://esthetic-serverside-teal.vercel.app/api/update/${orderId}`
-          )
-          .then(res => {
-            if (res.status === 200) {
-              Swal.fire({
-                title: 'Accept!',
-                text: 'Your Appointment has been Accept.',
-                icon: 'success'
-              })
-            } else {
-              Swal.fire({
-                title: 'Error!',
-                text: 'Your Appointment not Accept.',
-                icon: 'warning'
-              })
-            }
-          })
-          .catch(err => {
-            console.error('Error aceepting order:', err)
-            // If error occurs, revert the local state update
-            setBookings(bookings)
-          })
-      }
-    })
-  }
-
-
-  const cancelStatus = orderId => {
-    Swal.fire({
-      title: 'Are you want to Cancel?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, Cancel it!'
-    }).then(result => {
-      if (result.isConfirmed) {
-        // Update the bookings state locally before making the API call
-        const updatedBookings = bookings.filter(order => order._id !== orderId)
-        setBookings(updatedBookings)
-
-        axios
-          .patch(
-            `https://esthetic-serverside-teal.vercel.app/api/update/${orderId}`
-          )
-          .then(res => {
-            if (res.status === 200) {
-              Swal.fire({
-                title: 'Cencel!',
-                text: 'Your Appointment has been Cancel.',
-                icon: 'success'
-              })
-            } else {
-              Swal.fire({
-                title: 'Error!',
-                text: 'Your Appointment not Cencel.',
-                icon: 'warning'
-              })
-            }
-          })
-          .catch(err => {
-            console.error('Error canceling order:', err)
-            // If error occurs, revert the local state update
-            setBookings(bookings)
-          })
-      }
-    })
-  }
+  //       Swal.fire({
+  //         title: 'Accept!',
+  //         text: 'Your file has been Accept.',
+  //         icon: 'success'
+  //       })
+  //     }
+  //   })
+  // }
 
   return (
     <div className='mt-[30%] md:mt-[20%] lg:mt-[15%] xl:mt-[10%] '>
@@ -177,7 +121,7 @@ function AdminDashboard () {
           </Link>
 
           <Link href='/adminAccepted'>
-            <button className='text-sm font-medium p-3  bg-green-500 text-white  rounded-lg my-2 lg:my-5 flex items-center gap-2 cursor-pointer'>
+            <button className='text-sm font-medium p-3  bg-green-400 text-white  rounded-lg my-2 lg:my-5 flex items-center gap-2 cursor-pointer'>
               {' '}
               <span>
                 <VscCheckAll />
@@ -199,39 +143,33 @@ function AdminDashboard () {
         ) : (
           <div className='grid lg:grid-cols-2 xl:grid-cols-3 gap-6 mb-28 lg:mb-20'>
             {bookings?.map((order, i) => (
-              <div key={i} className='bg-white border p-6 relative'>
-                <div className='grid grid-cols-2   border-b py-5'>
-                  <div className='  '>
-                    <h3>{order?.customerName}</h3>
-                    <p>{order?.phone}</p>
-                    <p>{order?.email}</p>
-                  </div>
-
-                  <div className=' flex justify-end'>
-                    <div className='space-x-2 text-sm'>
-                      {/* Delete Button */}
-                      <button
-                        onClick={() => deleteOrder(order._id)}
-                        className=' py-1 px-4  border rounded  hover:text-red-500 border-red-500 '
-                      >
-                        Delete
-                      </button>
-
-                      <button
-                        onClick={() => updateStatus(order._id)}
-                        className=' py-1 px-4    border border-green-500 rounded text-gray-800'
-                      >
-                        Accept
-                      </button>
-                      <button
-                        onClick={() => cancelStatus(order._id)}
-                        className=' py-1 px-4   border border-red-500 rounded  text-gray-800'
-                      >
-                        Cancel
-                      </button>
-                    </div>
-                  </div>
+              <div
+                key={i}
+                className='bg-white border border-green-600 p-6 relative'
+              >
+                <div className='pb-5 border-b '>
+                  <h3>
+                    {order?.customerName}{' '}
+                    <span className='text-green-600 text-sm'>" Accepted "</span>{' '}
+                  </h3>
+                  <p>{order?.phone}</p>
+                  <p>{order?.email}</p>
                 </div>
+
+                {/* Delete Button */}
+                <button
+                  onClick={() => deleteOrder(order._id)}
+                  className='absolute top-5 right-8 text-red-500 hover:text-red-700'
+                >
+                  <FaTrash size={18} />
+                </button>
+
+                <button
+                  // onClick={() => updateStatus(order._id)}
+                  className='absolute top-16 right-8  text-gray-800'
+                >
+                  <PiChecksBold size={18} />
+                </button>
 
                 {/* Booking Details */}
                 <div className='border-b py-4 mb-4'>
@@ -245,41 +183,38 @@ function AdminDashboard () {
                     <span>
                       <LuAlarmClock />
                     </span>{' '}
-                    Time: {order.time}
+                    Time: {order?.time}
                   </p>
                 </div>
 
                 {/* Services */}
                 <div>
                   <h4 className='text-lg font-semibold mb-2 text-gray-600'>
-                    Selected Services:{' '}
-                    <span className='text-sm text-red-500'>
-                      ({order?.status})
-                    </span>
+                    Selected Services:
                   </h4>
                   <div className='space-y-4'>
-                    {order.selectedServices.map(service => (
+                    {order?.selectedServices?.map(service => (
                       <div
-                        key={service.id}
+                        key={service?.id}
                         className='flex items-center gap-4 p-3 border rounded-lg bg-gray-50'
                       >
                         <div>
                           <p className='text-gray-600 font-medium'>
-                            {service.serviceName}
+                            {service?.serviceName}
                           </p>
                           <div className='flex gap-x-5'>
                             <p className='text-gray-600 text-sm flex items-center gap-2'>
                               <span>
                                 <LuAlarmClock />
                               </span>{' '}
-                              {service.time}
+                              {service?.time}
                             </p>
                             <p className='text-gray-700'>
                               <span className='line-through text-gray-500'>
-                                ${service.originalPrice}
+                                ${service?.originalPrice}
                               </span>{' '}
                               <span className='text-green-600 font-bold'>
-                                ${service.discountedPrice}
+                                ${service?.discountedPrice}
                               </span>
                             </p>
                           </div>
@@ -293,7 +228,7 @@ function AdminDashboard () {
                 <div className='mt-4 text-right'>
                   <p className='text-lg font-semibold text-gray-600'>
                     Total:{' '}
-                    <span className='text-green-600'>${order.subtotal}</span>
+                    <span className='text-green-600'>${order?.subtotal}</span>
                   </p>
                 </div>
               </div>
@@ -305,4 +240,4 @@ function AdminDashboard () {
   )
 }
 
-export default AdminDashboard
+export default AdminCancel ;
